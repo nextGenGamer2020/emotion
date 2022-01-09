aa8421542c62e836b59f5c9093258305cd2594d9
nextGenGamer2020 <85717794+nextGenGamer2020@users.noreply.github.com>
2022-01-09 17:45:45 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function take_snapshot()
 }
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/A_3kzjAmg/model.JSON', modelLoaded)
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/A_3kzjAmg/model.json', modelLoaded)
 
 function modelLoaded() {
     console.log('Model Loaded!')
